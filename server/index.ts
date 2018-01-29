0f16f25588c69eeccc4e4e2cde41301a114119eb
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-01-28 23:55:14 -0500

    Bare index file for server

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import {ServerApp} from './server-app'
+
+import winston=require('winston');
+require('dotenv').config();
+winston.level='debug';
+
+winston.info("Initializing Server");
+
+//start the server (the db field of the server will be empty)
+winston.info("Starting server without a database");
+let server=new ServerApp();
+server.setRoutes();
+server.start();
+
