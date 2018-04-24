f440c76e0ef8a9391f8d2e3d785906d306b97038
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-04-25 00:00:04 +0530

    Installed type definitions for some dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import {ServerApp} from './server-app';
-
+import { connectToDatabase,DatabaseOptions } from './database-connection';
 import winston=require('winston');
 require('dotenv').config();
 winston.level='debug';
@@ -12,3 +12,44 @@ let server=new ServerApp();
 server.setRoutes();
 server.start();
 
+
+let iWantToUseADatabase=true;
+
+if(iWantToUseADatabase){//configure and setup OrientDB database before launching the server
+	let databaseOptions=new DatabaseOptions();//<--there are a total of 7 properties
+	databaseOptions.host=process.env.DB_HOST;
+	databaseOptions.port=process.env.DB_PORT;
+	databaseOptions.username=process.env.DB_USERNAME;
+	databaseOptions.password=process.env.DB_PASSWORD;
+	databaseOptions.name=process.env.DB_NAME;
+	//change or add more properties as needed
+
+	connectToDatabase(databaseOptions).
+	//uncomment this part if you want to destroy and recreate the entire database schema from scratch
+	// then((db:ojs.Db)=>{
+	// 	winston.info("Databse connection established, destroying schema setup");
+	// 	return new SchemaBackend(db).dropDatabaseSchema().then((v:any)=>{
+	// 		return db;
+	// 	});
+	// }).
+	then((db:ojs.Db)=>{
+		winston.info("Databse connection established, ensuring schema setup");
+		return new SchemaBackend(db).ensureDatabaseSchema().then((v:any)=>{
+			winston.info("Schema ready");
+			return db;
+		});
+	}).then((db:ojs.Db)=>{
+		winston.info("Firing up server");
+		let server=new ServerApp(db);
+		server.setRoutes();
+		server.start();
+	}).catch((error:Error)=>{
+		winston.error("Problems occured during server initialization:"+error.message);
+	});
+}else{
+	//start the server (the db field of the server will be empty)
+	winston.info("Starting server without a database");
+	let server=new ServerApp();
+	server.setRoutes();
+	server.start();
+}
---
+++ server/typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/winston/index.d.ts" />
