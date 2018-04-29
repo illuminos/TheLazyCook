d16b3311e2b13b51eca46aee362b6d3cc7ba0bde
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-04-29 23:49:59 +0530

    Installed type definitions of server dependencies

--- server/typings/index.d.ts
+++ server/typings/index.d.ts
@@ -1,3 +1,7 @@
+/// <reference path="globals/express-session/index.d.ts" />
 /// <reference path="globals/orientjs/index.d.ts" />
 /// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/multer/index.d.ts" />
 /// <reference path="modules/winston/index.d.ts" />
